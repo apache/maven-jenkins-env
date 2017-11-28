1da40a476141139e51f1ed8a40cc1e36db090111
Stephen Connolly <stephen.alan.connolly@gmail.com>
2017-11-28 19:25:36 +0000

    Let's see if this works

--- vars/jenkinsNotify.groovy
+++ vars/jenkinsNotify.groovy
@@ -1,3 +1,40 @@
 #!/usr/bin/env groovy
 def call(Map params = [:]) {
+    def providers
+    def messageBody
+    def messageSubject
+    switch(currentBuild.currentResult) {
+        case "SUCCESS":
+            providers = []
+            messageSubject = "Build succeeded in Jenkins: ${currentBuild.fullDisplayName}"
+            messageBody = """See ${currentBuild.absoluteUrl}"""
+        break
+        case "UNSTABLE":
+            providers = [[$class: 'CulpritsRecipientProvider']]
+            messageSubject = "Build unstable in Jenkins: ${currentBuild.fullDisplayName}"
+            messageBody = """See ${currentBuild.absoluteUrl}"""
+        break
+        case "FAILURE":
+            providers = [[$class: 'CulpritsRecipientProvider']]
+            messageSubject = "Build failed in Jenkins: ${currentBuild.fullDisplayName}"
+            messageBody = """See ${currentBuild.absoluteUrl}"""
+        break
+        case "ABORTED":
+            providers = [[$class: 'CulpritsRecipientProvider']]
+            messageSubject = "Build aborted in Jenkins: ${currentBuild.fullDisplayName}"
+            messageBody = """See ${currentBuild.absoluteUrl}"""
+        break
+        default:
+        // should never happen if we are actually being invoked.
+        return
+    }
+    if (changeSets.empty) {
+        messageBody = messageBody+"\n\nNo changes.\n";
+    } else {
+        messageBody = messageBody + "\n\nChanges:\n";
+        for (def change in currentBuild.changeSets) {
+            messageBody = messageBody + "\n* ${change.msg}"
+        }
+    }
+    emailext body: messageBody, recipientProviders: providers, replyTo: 'dev@maven.apache.org', subject: messageSubject, to: 'notifications@maven.apache.org'
 }
