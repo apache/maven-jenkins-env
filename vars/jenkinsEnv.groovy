08b7d0c53872db8a23683cf05a0cba956cad0ea6
Stephen Connolly <stephen.alan.connolly@gmail.com>
2017-12-11 13:51:17 +0000

    Typowq

--- vars/jenkinsEnv.groovy
+++ vars/jenkinsEnv.groovy
@@ -23,7 +23,7 @@ class jenkinsEnv implements Serializable {
     def labelForOS(String os) {
         switch (os) {
             case 'linux':
-                return 'ubuntui && !ubuntu-4' // ubuntu-4 is bad: INFRA-15651
+                return 'ubuntu && !ubuntu-4' // ubuntu-4 is bad: INFRA-15651
             case 'windows':
                 return 'Windows'
             default:
