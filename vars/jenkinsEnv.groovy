e29308075d3807040ab7a1edd0d171788929c630
Karl Heinz Marbaise <khmarbaise@apache.org>
2018-08-01 21:46:47 +0200

    H31 seemed to have issues.

--- vars/jenkinsEnv.groovy
+++ vars/jenkinsEnv.groovy
@@ -23,7 +23,7 @@ class jenkinsEnv implements Serializable {
     def nodeSelection(String osLabel) {
 	  // exclude problematic nodes
 	  // windows-2012-1 Java 11 is a JRE, not a JDK
-	  return "\"${osLabel}\""
+	  return "\"${osLabel}\" && !H31"
 	}
 
     def labelForOS(String os) {
