db317d6dc1ecb381c0089c7c1e849d264ba259a7
Karl Heinz Marbaise <khmarbaise@apache.org>
2018-08-25 17:47:30 +0200

    Removed exclusion of H31.

--- vars/jenkinsEnv.groovy
+++ vars/jenkinsEnv.groovy
@@ -23,7 +23,7 @@ class jenkinsEnv implements Serializable {
     def nodeSelection(String osLabel) {
 	  // exclude problematic nodes
 	  // windows-2012-1 Java 11 is a JRE, not a JDK
-	  return "\"${osLabel}\" && !H31"
+	  return "\"${osLabel}\""
 	}
 
     def labelForOS(String os) {
