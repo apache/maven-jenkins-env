c342b841165096210042b0000a07a735250e9823
rfscholte <rfscholte@apache.org>
2019-09-29 21:43:28 +0200

    Issues with H28 too

--- vars/jenkinsEnv.groovy
+++ vars/jenkinsEnv.groovy
@@ -27,7 +27,7 @@ class jenkinsEnv implements Serializable {
     def labelForOS(String os) {
         switch (os) {
             case 'linux':
-                return 'ubuntu && !H29'
+                return 'ubuntu && !H28 && !H29'
             case 'windows':
 //                return 'windows-2016-3'
                 return 'Windows'
