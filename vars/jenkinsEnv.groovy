9b9b33766b20a82229009eea0b71b4b9e8cf5405
rfscholte <rfscholte@apache.org>
2020-01-17 10:48:56 +0100

    H30 is having issues

--- vars/jenkinsEnv.groovy
+++ vars/jenkinsEnv.groovy
@@ -27,7 +27,7 @@ class jenkinsEnv implements Serializable {
     def labelForOS(String os) {
         switch (os) {
             case 'linux':
-                return 'ubuntu && !H30'
+                return 'ubuntu && !H29'
             case 'windows':
 //                return 'windows-2016-3'
                 return 'Windows'
