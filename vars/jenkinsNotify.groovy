c963ecde67e7821fad140088124b442b1daac7f5
rfscholte <rfscholte@apache.org>
2021-07-16 20:30:27 +0200

    disable jiraIssueSelector

--- vars/jenkinsNotify.groovy
+++ vars/jenkinsNotify.groovy
@@ -65,23 +65,23 @@ def call(Map params = [:]) {
     // comment on any jira tickets
     def jiraIssues = null
     def jiraMavens = []
-    try {
-        jiraIssues = jiraIssueSelector(issueSelector: [$class: 'DefaultIssueSelector'])
-        for (def jiraIssue in jiraIssues) {
-            // only comment on maven's issues, all our trackers start with M
-            // may end up commenting on other TLPs in some cases but should be very rare
-            if (jiraIssue.startsWith("M")) {
-                try {
-                    jiraComment body: "${messageSubject}\n\n${messageBody}", issueKey: jiraIssue
-                } catch (e) {
-                    echo "WARNING: Could not update ${jiraIssue}: ${e.message}"
-                }
-                jiraMavens += jiraIssue
-            }
-        }
-    } catch (e) {
-        echo "WARNING: Could not determine JIRA issues: ${e.message}"
-    }
+//    try {
+//        jiraIssues = jiraIssueSelector(issueSelector: [$class: 'DefaultIssueSelector'])
+//        for (def jiraIssue in jiraIssues) {
+//            // only comment on maven's issues, all our trackers start with M
+//            // may end up commenting on other TLPs in some cases but should be very rare
+//            if (jiraIssue.startsWith("M")) {
+//                try {
+//                    jiraComment body: "${messageSubject}\n\n${messageBody}", issueKey: jiraIssue
+//                } catch (e) {
+//                    echo "WARNING: Could not update ${jiraIssue}: ${e.message}"
+//                }
+//                jiraMavens += jiraIssue
+//            }
+//        }
+//    } catch (e) {
+//        echo "WARNING: Could not determine JIRA issues: ${e.message}"
+//    }
     // set the build description to the jira ticket id's
     if (!jiraMavens.empty) {
         currentBuild.description = "${jiraMavens.join(', ')}"
